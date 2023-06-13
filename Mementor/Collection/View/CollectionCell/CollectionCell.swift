42d3426182985c44554671638c42dcfce382e2f0
Evgeniy Kireichev <e.kireichev@tinkoff.ru>
2023-06-13 17:32:16 +0300

    Add appicon, update cell layout, update cover

--- Mementor/Collection/View/CollectionCell/CollectionCell.swift
+++ Mementor/Collection/View/CollectionCell/CollectionCell.swift
@@ -24,13 +24,18 @@ final class CollectionCell: UICollectionViewCell {
     }
 
     private func commonInit() {
+        cellImageView.layer.borderColor = Colors.text.cgColor
+        cellImageView.layer.borderWidth = 1.0
+        cellImageView.layer.cornerRadius = 24.0
+        cellImageView.clipsToBounds = true
+
         contentView.addSubview(cellImageView)
         cellImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            cellImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            cellImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            cellImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
-            cellImageView.rightAnchor.constraint(equalTo: contentView.rightAnchor)
+            cellImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4.0),
+            cellImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4.0),
+            cellImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 4.0),
+            cellImageView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -4.0)
         ])
     }
     
